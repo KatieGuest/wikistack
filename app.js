4192257e05c976476372e6fd9cb10507141954cd
Katie Guest <katieguest11@gmail.com>
2019-04-25 15:50:41 -0400

    up to HTML forms

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
-const layout = require('./views/layout')
+const layout = require('./views/layout');
 const { db } = require('./models');
 const models = require('./models');
+const wiki = require('./routes/wiki')
+const user = require('./routes/user')
 
 app.use(morgan('dev'));
 app.use(express.static(__dirname + "public"));
 app.use(express.urlencoded({ extended: false }));
+app.use('/wiki', wiki);
+app.use('/user', user);
 
 
 db.authenticate().
@@ -17,15 +21,13 @@ then(() => {
 
 
 app.get('/', (req, res) => {
-  res.send(layout(''))
+  res.redirect('/wiki')
 });
 
 const PORT = 3000;
 
 async function init(){
-  await models.Page.sync();
-  await models.User.sync();
-
+  await models.db.sync({force: true});
   app.listen(PORT, () => {
     console.log(`App listening in port ${PORT}`)
   })
---
+++ routes/wiki.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const router = express.Router();
+const addPage = require('../views/addPage')
+
+
+router.get('/', (req, res) => {
+ res.send('/');
+})
+
+router.post('/', (req, res) => {
+  res.send('/');
+ })
+
+router.get('/add', (req, res) => {
+  res.send(addPage());
+ })
+
+module.exports = router;
+
